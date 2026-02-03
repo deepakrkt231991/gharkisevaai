46ddf4f4a10384dcad96fcab7825d1b570f60887
Deepak Kadam <deepakkadamrkt@gmail.com>
2026-02-03 10:11:14 +0000

    Force dynamic explore page

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,10 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // इससे टाइपिंग के एरर बिल्ड नहीं रोकेंगे
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // इससे लिंटिंग के एरर बिल्ड नहीं रोकेंगे
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
-      }
-    ],
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  // यह लाइन जोड़ें
+  output: 'standalone', 
 };
 
 export default nextConfig;
